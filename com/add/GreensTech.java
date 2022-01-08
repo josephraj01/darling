f321a7a27037b46635f3e5e4c0045587a417aaed
josephfere <fereirojoseph@gmail.com>
2022-01-08 22:24:09 +0530

    jbfjb

---
+++ com/add/GreensTech.java
@@ -0,0 +1,19 @@
+package com.add;
+// child
+
+public class GreensTech {
+private void grreensOmr() {
+	// TODO Auto-generated method stub
+System.out.println("greens tech OMR");
+}
+private void greensAadyar() {
+	// TODO Auto-generated method stub
+System.out.println("greens adyar");
+}
+
+public static void main(String[] args) {
+	GreensTech ab=new GreensTech();
+	ab.greensAadyar();
+	ab.grreensOmr();
+}
+}
